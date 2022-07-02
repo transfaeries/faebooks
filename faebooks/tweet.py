fc5fe8d93b47c39d098a5c4b90c74291a41f766f
transfaeries <transfaeries@gmail.com>
2022-07-02 10:26:09 -0400

    tweets at an interval achieved

--- faebooks/tweet.py
+++ faebooks/tweet.py
@@ -1,21 +1,35 @@
+## Good Morning, welcome to faebot
+# Faebot's is a bot who is also a faerie
+
+import os
+import time
+import logging
+from random import randrange
 from twitter import *
 import openai
-import os
 
+# set up logging
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
+
+# this loads all of the secrets
 openai.api_key = os.getenv("OPENAI_API_KEY", "")
-
-
-token = os.getenv("TWITTER_ACCESS_TOKEN_KEY","")
+token = os.getenv("TWITTER_ACCESS_TOKEN","")
 token_secret = os.getenv("TWITTER_ACCESS_SECRET","")
-consumer_key = os.getenv("TWITTER_CONSUMER_KEY","")
-consumer_secret = os.getenv("TWITTER_CONSUMER_SECRET","")
+api_key = os.getenv("TWITTER_API_KEY","")
+api_secret = os.getenv("TWITTER_API_SECRET","")
 model = os.getenv("MODEL_NAME", "curie")
 
+#set up the twitter connection
 t = Twitter(
-    auth=OAuth(token, token_secret, consumer_key, consumer_secret))
+    auth=OAuth(token, token_secret, api_key, api_secret))
 
+# This will seed the model:
 tweet_prompt = "The following is a tweet by faebot, a bot that is also a faerie:"
 
+#Prompts Open AI for a tweet
 def generate(prompt: str = "") -> str:
     response = openai.Completion.create(  # type: ignore
         engine=model,
@@ -30,7 +44,11 @@ def generate(prompt: str = "") -> str:
     return response["choices"][0]["text"].strip()
 
 if __name__=="__main__":
-    # t.statuses.update(status="I live again, my sci-fi heart could melt. Anyone remember Silver Dream... Free View in")
-    message = generate(tweet_prompt)
-    print(message)
-    t.statuses.update(status=message)
+    while True:
+        message = generate(tweet_prompt)
+        logging.info("MESSAGE TO POST: "+message)
+        t.statuses.update(status=message)
+        # sleep for 5 mins to 2 hours before posting again
+        sleeptime=randrange(300,7200)
+        logging.info(f"sleeping for {sleeptime} seconds before posting again.")
+        time.sleep(sleeptime)
