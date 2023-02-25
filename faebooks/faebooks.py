532ff43413bfe00703d6290aa5fe7a3073dfd175
transfaeries <contact@transfaerie.com>
2023-02-25 14:56:46 -0500

    added and ran black

--- faebooks/faebooks.py
+++ faebooks/faebooks.py
@@ -1,8 +1,8 @@
 ## Good Morning, welcome to faebot
 # Faebot's is a bot who is also a faerie
-#Faebot uses Open.ai gpt to generate posts as well as have conversations
+# Faebot uses Open.ai gpt to generate posts as well as have conversations
 
-# Faebot will post a new post at random intervals during the day 
+# Faebot will post a new post at random intervals during the day
 
 import os
 import sys
@@ -17,10 +17,10 @@ import signal
 
 # set up logging
 logging.basicConfig(
-    format='%(asctime)s %(levelname)-8s %(message)s',
+    format="%(asctime)s %(levelname)-8s %(message)s",
     level=logging.INFO,
-    datefmt='%Y-%m-%d %H:%M:%S')
-
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
 
 
 # This will seed the model:
@@ -28,35 +28,28 @@ post_prompt = ""
 model = os.getenv("MODEL_NAME", "curie")
 
 
-
 class Faebooks:
     def __init__(self) -> None:
         # this loads all of the secrets
         openai.api_key = os.getenv("OPENAI_API_KEY", "")
-        token = os.getenv("TWITTER_ACCESS_TOKEN","")
-        token_secret = os.getenv("TWITTER_ACCESS_SECRET","")
-        api_key = os.getenv("TWITTER_API_KEY","")
-        api_secret = os.getenv("TWITTER_API_SECRET","")
-        masto_token = os.getenv("MASTO_ACCESS_TOKEN","")
-        masto_url = os.getenv("MASTO_BASE_URL","")
+        token = os.getenv("TWITTER_ACCESS_TOKEN", "")
+        token_secret = os.getenv("TWITTER_ACCESS_SECRET", "")
+        api_key = os.getenv("TWITTER_API_KEY", "")
+        api_secret = os.getenv("TWITTER_API_SECRET", "")
+        masto_token = os.getenv("MASTO_ACCESS_TOKEN", "")
+        masto_url = os.getenv("MASTO_BASE_URL", "")
 
-        #set up the twitter connection
-        self.twitter = Twitter(
-            auth=OAuth(token, token_secret, api_key, api_secret))
-        
-        #set up the mastodon connection
-        self.mastodon = Mastodon(
-            access_token=masto_token,
-            api_base_url=masto_url
-        )
+        # set up the twitter connection
+        self.twitter = Twitter(auth=OAuth(token, token_secret, api_key, api_secret))
 
-        #additional setup
+        # set up the mastodon connection
+        self.mastodon = Mastodon(access_token=masto_token, api_base_url=masto_url)
+
+        # additional setup
         self.exiting = False
         self.sigints = 0
 
-
-
-    #Prompts Open AI for a post
+    # Prompts Open AI for a post
     async def generate(self, prompt: str = "") -> str:
         response = openai.Completion.create(  # type: ignore
             engine=model,
@@ -86,15 +79,14 @@ class Faebooks:
         max_restarts = 15
         while self.sigints == 0 and not self.exiting:
             try:
-
                 await self.post()
-                sleeptime=randrange(3000,17200)
+                sleeptime = randrange(3000, 17200)
                 logging.info(f"sleeping for {sleeptime} seconds before posting again.")
                 await asyncio.sleep(sleeptime)
 
             except Exception as e:
                 logging.error(e)
-                backoff = 2 ** restart_count
+                backoff = 2**restart_count
                 if restart_count >= max_restarts:
                     logging.error("max restarts reached, exiting")
                     sys.exit(1)
@@ -102,11 +94,7 @@ class Faebooks:
                 await asyncio.sleep(backoff)
                 restart_count += 1
 
-            logging.info(
-                "started faebot @ faebot_01"
-            )
-    
-
+            logging.info("started faebot @ faebot_01")
 
     def sync_signal_handler(self, *_: any) -> None:
         """Try to start async_shutdown and/or just sys.exit"""
@@ -125,12 +113,12 @@ class Faebooks:
     async def async_shutdown(self) -> None:
         """Shutdown the bot"""
         logging.info("shutting down")
-        tasks = [ t for t in asyncio.all_tasks() if t is not asyncio.current_task() ]
+        tasks = [t for t in asyncio.all_tasks() if t is not asyncio.current_task()]
         for task in tasks:
             task.cancel()
         sys.exit(0)
 
-    
-if __name__=="__main__":
+
+if __name__ == "__main__":
     faebot = Faebooks()
     asyncio.run(faebot.start())
